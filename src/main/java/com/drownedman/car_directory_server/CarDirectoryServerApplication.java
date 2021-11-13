86dfcbfeda29108c0b6019d694420962f82b582c
Danila <52175631+Dannik7771@users.noreply.github.com>
2021-11-13 11:05:08 +0300

    init commit

---
+++ src/main/java/com/drownedman/car_directory_server/CarDirectoryServerApplication.java
@@ -0,0 +1,13 @@
+package com.drownedman.car_directory_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDirectoryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDirectoryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drownedman/car_directory_server/CarDirectoryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drownedman.car_directory_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDirectoryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
